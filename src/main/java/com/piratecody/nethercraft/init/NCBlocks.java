f9d775249b1b69f6c07e87f6fd661b799788df84
PirateCody <piratecody16@gmail.com>
2015-08-22 15:29:23 -0500

    Added more ores

--- src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
+++ src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
@@ -9,10 +9,14 @@ import net.minecraft.block.material.Material;
 public class NCBlocks {
 	
 	public static Block corruptIronOre = new BlockNCOre("CorruptIronOre", Material.rock, 2.0F, 4.0F, 0.2F, "pickaxe", 2);
+	public static Block corruptDiamondOre = new BlockNCOre("CorruptDiamondOre", Material.rock, 4.0F, 4.32F, 0.4F, "pickaxe", 3);
+	public static Block obsidianOre = new BlockNCOre("ObsidianOre", Material.rock, 5.0F, 20.0F, 0F, "pickaxe", 3);
 	
 	public static void registerBlocks(){
 		
 		RegisterHelper.registerBlock(corruptIronOre);
+		RegisterHelper.registerBlock(corruptDiamondOre);
+		RegisterHelper.registerBlock(obsidianOre);
 		
 	}
 
