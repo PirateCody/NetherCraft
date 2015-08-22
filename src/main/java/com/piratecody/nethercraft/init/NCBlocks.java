f076cc76881514b778ca0436ea42b1bc145b0f1d
PirateCody <piratecody16@gmail.com>
2015-08-22 15:22:30 -0500

    Added Corrupt Iron Ore

--- src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
+++ src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
@@ -1,11 +1,18 @@
 package com.piratecody.nethercraft.init;
 
+import com.piratecody.nethercraft.blocks.BlockNCOre;
+import com.piratecody.nethercraft.helpers.RegisterHelper;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
 public class NCBlocks {
 	
-	
+	public static Block corruptIronOre = new BlockNCOre("CorruptIronOre", Material.rock, 2.0F, 4.0F, 0.2F, "pickaxe", 2);
 	
 	public static void registerBlocks(){
 		
+		RegisterHelper.registerBlock(corruptIronOre);
 		
 	}
 
