c791fcae1d951fe9da414179667ceea13c88a0b9
PirateCody <piratecody16@gmail.com>
2015-08-28 10:09:04 -0500

    Mob gen in nether biome

--- src/main/java/com/piratecody/nethercraft/generation/BiomeGenNether.java
+++ src/main/java/com/piratecody/nethercraft/generation/BiomeGenNether.java
@@ -1,14 +1,17 @@
 package com.piratecody.nethercraft.generation;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.monster.EntityGhast;
+import net.minecraft.entity.monster.EntityMagmaCube;
+import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class BiomeGenNether extends BiomeGenBase{
 
 	
 	public final Material blockMaterial;
+	@SuppressWarnings("unchecked")
 	public BiomeGenNether(int par1)
 	{
 	super(par1);
@@ -20,6 +23,11 @@ public class BiomeGenNether extends BiomeGenBase{
 	this.topBlock = Blocks.netherrack;
 	this.fillerBlock = Blocks.netherrack;
 	this.setBiomeName("Tutorial");
+	
+	this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityGhast.class, 50, 4, 4));
+    this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityPigZombie.class, 100, 4, 4));
+    this.spawnableMonsterList.add(new BiomeGenBase.SpawnListEntry(EntityMagmaCube.class, 1, 4, 4));
+
 
 	/** this changes the water colour, its set to red now but ggole the java colours **/
 	this.waterColorMultiplier = 0xE42D17;
