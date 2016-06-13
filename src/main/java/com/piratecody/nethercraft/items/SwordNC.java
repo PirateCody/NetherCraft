d4b2f9134ee90708d57b8df35fb723c811f188a2
PirateCody <piratecody16@gmail.com>
2016-06-13 13:05:11 -0500

    Playing around with hitEntity()

--- src/main/java/com/piratecody/nethercraft/items/SwordNC.java
+++ src/main/java/com/piratecody/nethercraft/items/SwordNC.java
@@ -6,15 +6,19 @@ import com.piratecody.nethercraft.init.NCTabs;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 
 public class SwordNC extends ItemSword {
 
+	private String name;
 	public SwordNC(ToolMaterial material, String name) {
 		super(material);
 		this.setUnlocalizedName(name);
 		this.setTextureName(Reference.MODID + ":" + name);
 		this.setCreativeTab(NCTabs.tabNC);
+		this.name = name;
 	}
 	
 	@Override
@@ -22,6 +26,18 @@ public class SwordNC extends ItemSword {
 	public void registerIcons(IIconRegister par1IconRegister)
 	{
 		this.itemIcon = par1IconRegister.registerIcon(Reference.MODID + ":" + getUnlocalizedName().substring(5));
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack item, EntityLivingBase enemy, EntityLivingBase player)
+	{
+		
+		if(name == "CorruptDiamondSword")
+		{
+			item.damageItem(1, player);
+			enemy.addVelocity(0, 0.5, 0);
+		}
+		return true;
 	}
 
 
