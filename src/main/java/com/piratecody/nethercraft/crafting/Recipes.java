f7146fcd6491ed2a421a09e93e73fbd3c3088a4e
PirateCody <piratecody16@gmail.com>
2016-06-14 13:51:11 -0500

    Smelting CorruptIronOre yields 2 ingots

--- src/main/java/com/piratecody/nethercraft/crafting/Recipes.java
+++ src/main/java/com/piratecody/nethercraft/crafting/Recipes.java
@@ -1,10 +1,33 @@
 package com.piratecody.nethercraft.crafting;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.item.ItemStack;
+
+import com.piratecody.nethercraft.init.*;
+
 public class Recipes {
+	
+	public static void addShapedRecipes()
+	{
+		
+	}
+	
+	public static void addShaplessRecipes()
+	{
+		
+	}
+	
+	public static void addSmeltingRecipes()
+	{
+		GameRegistry.addSmelting(NCBlocks.oreCorruptIron, new ItemStack(NCItems.ingotCorruptIron, 2), 2.5F);
+	}
 	
 	public static void registerRecipes()
 	{
-		//TODO add recipes
+		addShapedRecipes();
+		addShaplessRecipes();
+		addSmeltingRecipes();
+		
 	}
 
 }
