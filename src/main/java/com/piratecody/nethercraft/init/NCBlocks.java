e4b723a2798e3585baa9403b0b02b915172f1311
PirateCody <piratecody16@gmail.com>
2016-06-16 20:32:32 -0500

    Added corrupt obsidian ore

--- src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
+++ src/main/java/com/piratecody/nethercraft/init/NCBlocks.java
@@ -13,6 +13,7 @@ public class NCBlocks {
 	public static Block oreCorruptDiamond = new BlockNC("CorruptDiamondOre", Material.rock, 4.0F, 4.32F, 0.4F, "pickaxe", 3);
 	public static Block oreObsidian = new BlockNC("ObsidianOre", Material.rock, 5.0F, 20.0F, 0F, "pickaxe", 3);
 	public static Block oreEbony = new BlockNC("EbonyOre", Material.rock, 5.0F, 20.0F, 0F, "pickaxe", 3);
+	public static Block oreCorruptObsidian = new BlockNC("CorruptObsidianOre", Material.rock, 5.0F, 20.0F, 0F, "pickaxe", 3);
 	
 	public static Block blockCorruptIron = new BlockNC("CorruptIronBlock", Material.iron, 3.0F, 7.0F, 0F, "pickaxe", 2);
 	public static Block blockCorruptDiamond = new BlockNC("CorruptDiamondBlock", Material.coral, 4.0F, 7.1F, 0F, "pickaxe", 2);
@@ -28,6 +29,7 @@ public class NCBlocks {
 		RegisterHelper.registerBlock(blockEbony);
 		RegisterHelper.registerBlock(blockCorruptIron);
 		RegisterHelper.registerBlock(blockCorruptDiamond);
+		RegisterHelper.registerBlock(oreCorruptObsidian);
 		
 	}
 
